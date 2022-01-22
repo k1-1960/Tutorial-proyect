75bf0e1bbe1eb1fbe4a0ad49482e6cfccb4ae56d
K1 <92892672+k1-1960@users.noreply.github.com>
2022-01-21 22:16:32 -0700

    Update send_error.js

--- funciones_mias/send_error.js
+++ funciones_mias/send_error.js
@@ -1,15 +1,14 @@
-const got = require('got');
-
 module.exports.send_error = async (client, owner, error) => {
-
-let body = await got("https://showcase.api.linx.twenty57.net/UnixTime/tounixtimestamp?datetime=now");
-  
+ try{
   client.users.cache.get(owner).send({ embeds: [{
     "title": "Error",
-    "description": `${error}\nA las \`<t:${body["UnixTimeStamp"]}:t>\``,
+    "description": `${error}`,
     "color": "#dd0000"
   }]
  })
+} catch (err) {
+ console.log("[SEND_ERROR] -- Owner con dm cerrado o id incorrecto");
+}
 
   return;
 }
