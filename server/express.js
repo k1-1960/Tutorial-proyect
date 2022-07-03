42dc43fd6770b3d946a16cfbdff6fe9cbccf917f
K1 <k1.codingworld@gmail.com>
2022-07-02 19:12:41 -0600

    Update express.js

--- server/express.js
+++ server/express.js
@@ -4,7 +4,7 @@ const path = require("path");
 const app = express();
 const port = 3310;
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, 'home.html'));
+  res.send('Activo xd');
 });
 app.listen(port, () => console.log(`Code online 👍`));
 
